6e0412d5bf2df820d8b01bc60772b299de2f8f0c
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-08 21:27:55 +0800

    Update src RandomForest

--- src/RandomForest/Training.py
+++
@@ -1,9 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-
-rf = RandomForestClassifier(
-    n_estimators=100,
-    random_state=42,
-    class_weight='balanced' #Otomatis beri bobot pada kelas minoritas
-)
-rf.fit(x_train, y_train)
-y_pred = rf.predict(x_test)
