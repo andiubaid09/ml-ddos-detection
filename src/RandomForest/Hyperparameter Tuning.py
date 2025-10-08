4fa8a3779c9dce273d3b39d82a9e489ad0610c6f
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-08 21:24:59 +0800

    Update src Hyperparameter Tuning.py

---
+++ src/RandomForest/Hyperparameter Tuning.py
@@ -0,0 +1,26 @@
+from scipy.stats import randint
+
+param_distributions = {
+    'classifier__n_estimators': randint(100,200,300),
+    'classifier__max_depth': randint(4, 5, 10),
+    'classifier__min_samples_split': randint(2, 5, 10),
+    'classifier__min_samples_leaf': randint(1, 3, 10),
+    'classifier__max_features': ['sqrt', 'log2', None],
+    'classifier__bootstrap': [True,False]
+}
+
+from sklearn.model_selection import RandomizedSearchCV, StratifiedKFold
+cv = StratifiedKFold(n_splits=5 , shuffle=True, random_state=42)
+random_search = RandomizedSearchCV(
+    estimator=rf_pipeline,
+    param_distributions=param_distributions,
+    n_iter = 30,
+    cv = cv,
+    scoring = 'accuracy',
+    n_jobs=-1,
+    verbose=2,
+    random_state=42
+)
+random_search.fit(X_train, y_train)
+print('Best parameters', random_search.best_params_)
+print('Best CV Score', random_search.best_score_)
