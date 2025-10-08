23aed72937a6b810f99ad049cef6b2fcfe63c7e8
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-08 17:10:49 +0800

    Update Preprocessing.py

--- src/XGBoost/Preprocessing.py
+++ src/XGBoost/Preprocessing.py
@@ -5,31 +5,36 @@ drive.mount ('/content/drive')
 
 datasheet = '/content/drive/My Drive/Datasheet/dataset_sdn.csv'
 df = pd.read_csv(datasheet)
-df.info()
-
-from sklearn.preprocessing import OneHotEncoder
-
-df['Protocol'] = df ['Protocol'].astype(str)
-encoder = OneHotEncoder(sparse_output=False)
-encoded_clean = encoder.fit_transform(df[['Protocol']]).astype(str)
-encoded_clean = pd.DataFrame(
-    encoded_clean,
-    columns= encoder.get_feature_names_out(['Protocol']),
-    index = df.index
-)
-df = pd.concat([df.drop(columns=['Protocol']), encoded_clean], axis = 1)
-df.columns
 Features = [
-    'dt','dur','dur_nsec','tot_dur','pktrate','Protocol_ICMP','Protocol_TCP','Protocol_UDP',
-    'port_no','tx_kbps','rx_kbps','tot_kbps'
+    'dt','dur','dur_nsec','tot_dur','pktrate','Protocol','port_no','tx_kbps','rx_kbps','tot_kbps'
 ]
 df_clean = df[Features]
 
 from sklearn.model_selection import train_test_split
 
-X = df_clean.astype('float32')
+X = df_clean
 y = df['label']
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size = 0.2, random_state=42, stratify =y
+    X, y, random_state=42
 )
-df['label'].value_counts()
+
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
+encoder_cat = ['Protocol']
+encoder_transform = encoder
+numeric_features = ['dt','dur','dur_nsec','tot_dur','pktrate','port_no','tx_kbps','rx_kbps','tot_kbps']
+scaler = StandardScaler()
+numeric_feat = numeric_features
+numeric_transform = scaler
+
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat',encoder_transform, encoder_cat),
+        ('num', numeric_transform, numeric_feat)
+    ],
+    remainder = 'drop'
+)
