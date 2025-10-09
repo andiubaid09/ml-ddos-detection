b168d2a4346f03b2e28bd33e6cefb1fd8d584c8a
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-09 18:50:59 +0800

    Update src Hyperparameter Tuning.py

--- src/CatBoost/Hyperparameter Tuning.py
+++ src/CatBoost/Hyperparameter Tuning.py
@@ -12,3 +12,22 @@ param_distri = {
     # Subsample rate
     'classifier__subsample': uniform(0.5,0.5),
 }
+
+from sklearn.model_selection import RandomizedSearchCV
+
+random_search = RandomizedSearchCV(
+    estimator = cat_pipeline,
+    param_distributions = param_distri,
+    n_iter = 15,
+    cv = 5,
+    scoring = 'accuracy',
+    verbose = 2,
+    random_state=42,
+    n_jobs=-1
+
+)
+# Memberitahu model CatBoost kolom kategorial dari pipeline [fitur hasil scaling numerik] + [fitur yang dipassthrough]
+cat_feature_indices = [len(Numeric_features)]  # kalau cuma 1 kolom kategori
+random_search.fit(X_train, y_train, classifier__cat_features=cat_feature_indices)
+print("Best params :", random_search.best_params_)
+print("Best CV Score: ", random_search.best_score_)
