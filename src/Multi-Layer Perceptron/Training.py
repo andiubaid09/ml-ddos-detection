7aeb9beddae67f19bba0bfedc9932c334bfad91b
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-10 18:19:37 +0800

    Update Training.py

--- src/Multi-Layer Perceptron/Training.py
+++ src/Multi-Layer Perceptron/Training.py
@@ -1,43 +1,9 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras import layers
-
-num_classes = 2
-model = keras.Sequential([
-    #Layer 1
-    layers.Dense(units=112, activation='relu', input_shape=(x_train_scaled.shape[1],)),
-    layers.Dropout(0.0), #No Dropout
-
-    #Layer 2
-    layers.Dense(units=128, activation='relu'),
-    layers.Dropout(0.1),
-
-    #Layer 3
-    layers.Dense(units=64, activation='tanh'),
-    layers.Dropout(0.0),
-
-    #Output Layers
-    layers.Dense(units=num_classes, activation='softmax')
-])
-
-learning_rate = 0.0009739768003064985
-
-model.compile(
-    optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
-    loss='sparse_categorical_crossentropy',
-    metrics=['accuracy']
+history = model.fit(
+    X_train_prep, y_train,
+    validation_split = 0.2,
+    epochs = 50,
+    validation_data = (X_val_prep, y_val),
+    verbose = 1
 )
-#Training Model MultiLayer Perceptron
-model.fit(
-    x_train_scaled,
-    y_train,
-    epochs=50,
-    validation_data=(x_test_scaled, y_test)
-)
-
-import numpy as np
-
-y_pred_prob = model.predict(x_test_scaled)
-y_pred = np.argmax(y_pred_prob, axis=1)
+test_loss, test_acc = model.evaluate(X_test_prep, y_test, verbose=0)
+print(f"Test loss: {test_loss:.4f} | Test acc :{test_acc:.4f}")
