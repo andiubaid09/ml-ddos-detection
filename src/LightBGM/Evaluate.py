ed72a0139d071c980935cb8e84c17c2e6e24967a
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-16 17:15:42 +0800

    Update src Evaluate

--- src/LightBGM/Evaluate.py
+++ src/LightBGM/Evaluate.py
@@ -1,8 +1,12 @@
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.model_selection import StratifiedKFold
+
 best_model_lbgm = random_searchlbgm.best_estimator_
 print(best_model_lbgm)
 
 # Pengujian menggunakan dataset training
-from sklearn.metrics import accuracy_score
 
 y_pred_train = best_model_lbgm.predict(X_train)
 acc_train = accuracy_score(y_train, y_pred_train)
@@ -10,20 +14,12 @@ print(f"Akurasi model dengan dataset training :{acc_train:.4f}")
 y_pred_test = best_model_lbgm.predict(X_test)
 acc_test = accuracy_score(y_test, y_pred_test)
 print(f"Akurasi model dengan dataset test : {acc_test:.4f}")
-
-from sklearn.metrics import confusion_matrix, classification_report
-
 print(classification_report(y_test, y_pred_test))
 
 # Validasi akurasi dengan cross validation
-from sklearn.model_selection import KFold, cross_val_score
-
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 scores = cross_val_score(best_model_lbgm, X,y, cv=kf)
 print(scores.mean())
-
-from sklearn.model_selection import StratifiedKFold
-
 skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 scores = cross_val_score(best_model_lbgm, X, y, cv=skf)
 print("Mean CV Accuracy:", scores.mean())
