087d94f752868c6ec93d9c3a84be576a96239452
Muhammad Andi Ubaidillah <muhandiubaid9601@gmail.com>
2025-10-16 17:27:14 +0800

    Update Preprocessing

--- src/RandomForest/Preprocessing.py
+++ src/RandomForest/Preprocessing.py
@@ -1,6 +1,11 @@
 from google.colab import drive
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+
 
 drive.mount('/content/drive')
 datasheet = '/content/drive/My Drive/Datasheet/DDoS/dataset_sdn.csv'
@@ -14,14 +19,11 @@ df.info()
 Features = ['dt','dur_nsec','tot_dur','pktrate','Protocol','port_no','tx_kbps','rx_kbps','tot_kbps']
 df_feat = df[Features]
 
-from sklearn.model_selection import train_test_split
 X = df_feat
 y = df['label']
 X_train, X_test, y_train, y_test = train_test_split(
     X,y, test_size=0.2, random_state=42, stratify=y
 )
-
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 numeric_features = ['dt','dur_nsec','tot_dur','pktrate','port_no','tx_kbps','rx_kbps','tot_kbps']
 kateogrial_features = ['Protocol']
@@ -32,8 +34,6 @@ scaler = StandardScaler()
 numeric_feat = numeric_features
 numeric_transform = scaler
 
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
 preprocessor = ColumnTransformer(
     transformers=[
         ('cat',encoder_transform, encoder_cat),
@@ -43,8 +43,6 @@ preprocessor = ColumnTransformer(
 )
 
 # Definisi base model
-from sklearn.ensemble import RandomForestClassifier
-
 model_rf = RandomForestClassifier(
     n_estimators=100,
     random_state=42,
